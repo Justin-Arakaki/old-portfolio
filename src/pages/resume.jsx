1dae2a399fff36cddad77b694b33a76b0adb3cd0
Justin-Arakaki <Justiniankoi@gmail.com>
2022-09-05 17:37:48 -0700

    Updated resume file

--- src/pages/resume.jsx
+++ src/pages/resume.jsx
@@ -13,7 +13,7 @@ export default function Resume(props) {
       </Typography>
       <Card sx={{ display: 'flex', justifyContent: 'center', p: '2rem' }}>
         <iframe
-          src="https://drive.google.com/file/d/1le8mzq0RoQL98GaaQGwit-JHSX1NRbi4/preview"
+          src="https://drive.google.com/file/d/1YRZl6cQM_mf4yzyiBJRSmJAFIHl88MMj/view"
           width="100%"
           height="500"
           allow="autoplay"
